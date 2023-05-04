93156d49d51ab94d212da92efec63078a74585db
Fahad_2022 <rjfahad44@gmail.com>
2023-05-04 22:28:05 +0600

    add project

---
+++ app/src/main/java/com/ft/ltd/jetpack_compose_login_ui/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ft.ltd.jetpack_compose_login_ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ltd.jetpack_compose_login_ui.ui.theme.JetpackComposeLoginUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeLoginUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeLoginUITheme {
+        Greeting("Android")
+    }
+}
